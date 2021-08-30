25ae9d59011cef4a001f7b2b4ee5206ba7c14da7
wonho <wonho1401@naver.com>
2021-08-30 17:42:22 +0900

    learn about IBAction , IBOutlet

--- UpdownGame/ViewController.swift
+++ UpdownGame/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
         print(sender.value)
     }
     //IBAction은 인터페이스 빌더에 요소들이 이벤트를 받았을 때 반응하기 위한 역할.
+    
     @IBAction func touchUpHitButton(_ sender:UIButton){
         print(slider.value)
     }
