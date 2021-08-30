3119744f770953b6ef5377f107559a5d6897de1d
wonho <wonho1401@naver.com>
2021-08-30 17:38:21 +0900

    IBAction and  IBOutlet

--- UpdownGame/ViewController.swift
+++ UpdownGame/ViewController.swift
@@ -8,13 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var slider: UISlider!
+        //outlet이란 것은 인터페이스 빌더에 올라와 있는 UI 요소의 값을 가져오는 역할.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // first commit
     }
-
-
+    
+    @IBAction func sliderValueChanged(_ sender:UISlider){
+        print(sender.value)
+    }
+    //IBAction은 인터페이스 빌더에 요소들이 이벤트를 받았을 때 반응하기 위한 역할.
+    @IBAction func touchUpHitButton(_ sender:UIButton){
+        print(slider.value)
+    }
+    
+    @IBAction func touchUpResetButton(_ sender:UIButton){
+        print("Touch up Reset Button")
+    }
 }
 
