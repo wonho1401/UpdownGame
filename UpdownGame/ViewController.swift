40c0282ad8d2d30cacf6986e583df57bfdf5f159
wonho <wonho1401@naver.com>
2021-09-03 14:14:33 +0900

    Declare Variables

--- UpdownGame/ViewController.swift
+++ UpdownGame/ViewController.swift
@@ -9,13 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var randomValue: Int = 0
+    var tryCount: Int = 0
     @IBOutlet weak var slider: UISlider!
         //outlet이란 것은 인터페이스 빌더에 올라와 있는 UI 요소의 값을 가져오는 역할.
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var minValueLabel: UILabel!
+    @IBOutlet weak var maxValueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb"), for: .normal)
+        reset()
     }
     
     @IBAction func sliderValueChanged(_ sender:UISlider){         
@@ -29,6 +36,21 @@ class ViewController: UIViewController {
     
     @IBAction func touchUpResetButton(_ sender:UIButton){
         print("Touch up Reset Button")
+        reset()
+    }
+    
+    func reset(){
+        randomValue = Int.random(in: 0...30)
+        print(randomValue)
+        tryCount = 0
+        tryCountLabel.text = "0/5"
+        slider.maximumValue = 30
+        slider.minimumValue = 0
+        slider.value = 15
+        minValueLabel.text = "0"
+        maxValueLabel.text = "30"
+        sliderValueLabel.text = "15"
+        
     }
 }
 
