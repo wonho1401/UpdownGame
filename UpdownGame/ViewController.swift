7c8e21131cd494403f4d2f839c7d86ca99ee277b
wonho <wonho1401@naver.com>
2021-09-06 16:56:42 +0900

    Add Alert

--- UpdownGame/ViewController.swift
+++ UpdownGame/ViewController.swift
@@ -27,11 +27,48 @@ class ViewController: UIViewController {
     
     @IBAction func sliderValueChanged(_ sender:UISlider){         
         print(sender.value)
+        let integerValue: Int = Int(slider.value)
+        sliderValueLabel.text = String(integerValue)
+        
     }
     //IBAction은 인터페이스 빌더에 요소들이 이벤트를 받았을 때 반응하기 위한 역할.
     
+    func showAlert(message: String){
+        
+        let alert = UIAlertController(title: nil,
+                                      message: message,
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            self.reset()
+        }
+        
+        alert.addAction(okAction)
+        present(alert,
+                animated: true,
+                completion: nil)
+    }
+    
     @IBAction func touchUpHitButton(_ sender:UIButton){
         print(slider.value)
+        let hitValue: Int = Int(slider.value)
+        slider.value = Float(hitValue)
+        
+        tryCount += 1
+        tryCountLabel.text = "\(tryCount)/5"
+        
+        if randomValue == hitValue {
+            showAlert(message: "YOU HIT!!")
+            reset()
+        }  else if tryCount >= 5{
+            showAlert(message: "YOU LOSE..")
+            reset()
+        } else if randomValue > hitValue {
+            slider.minimumValue = Float(hitValue)
+            minValueLabel.text = String(hitValue)
+        } else {
+            slider.maximumValue = Float(hitValue)
+            maxValueLabel.text = String(hitValue)
+        }
     }
     
     @IBAction func touchUpResetButton(_ sender:UIButton){
