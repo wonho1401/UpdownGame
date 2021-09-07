d98730873b4a7114aa127ab6072c9eefe74312d2
wonho <wonho1401@naver.com>
2021-09-07 11:23:17 +0900

    storyboard segue

---
+++ UpdownGame/CreditViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CreditViewController.swift
+//  UpdownGame
+//
+//  Created by 정원호 on 2021/09/07.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    @IBAction func touchUpDismissButton(_ sender:UIButton){
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
